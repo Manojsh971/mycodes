830e4af10009d73071b0e6d99c42a4c42679950c
Manoj S H <80801110+Manojsh971@users.noreply.github.com>
2022-01-18 19:32:44 +0530

    Add files via upload

---
+++ errorhandling.py
@@ -0,0 +1,16 @@
+while(True):
+    a=int(input("enter number1: "))
+    b=int(input("enter number2: "))
+    
+    try:
+        c=a/b
+        print(c)
+        
+    except Exception as e:
+        print("infiite value")
+        
+
+
+        
+        
+        
